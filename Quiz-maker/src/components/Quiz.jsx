24f9699e1468299c01b597455a2f18fb00de3463
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-08-17 22:18:46 +0530

    Internship Projects

--- Quiz-maker/src/components/Quiz.jsx
+++ Quiz-maker/src/components/Quiz.jsx
@@ -10,31 +10,39 @@ const Quiz = () => {
   // Locking the answer
   let [lock, setLock] = useState(false);
 
+  const [rightanswercolor, setrightanswercolor] = useState('')
+  const [wronganswercolor, setwronganswercolor] = useState('')
+
   // setting the score
-  const [score,setscore] = useState(0);
+  const [score, setscore] = useState(0);
+
+  let [result, setresult] = useState(false);
 
   const checkanswer = (e, ans) => {
     if (lock === false) {
       if (Answer === ans) {
-        e.target.classList.add("bg-green-500")
-        setscore(prev => prev+1);
+        setrightanswercolor('bg-green-500');
+        setscore(prev => prev + 1);
       }
       else {
-        e.target.classList.add('bg-red-400');
+        setrightanswercolor('bg-green-500');
+        setwronganswercolor('bg-red-400');
       }
       setLock(true)
     }
   }
 
   return (
-    <div className='flex flex-col gap-10 sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className='flex flex-col gap-10 sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden'>
       <h1 className='text-3xl font-semibold text-center border-b-4 border-blue-500/50'>
         <span className='text-blue-500'>Quiz App </span></h1>
-      <div className=''>
+      {!result 
+        ?
+        <div>
         <h1 className='text-xl md:text-3xl font-serif'>Question {index + 1} : {Question}</h1>
         <ul>
           {Options.map((item, idx) => (
-            <li key={idx} onClick={(e) => checkanswer(e, item)} className={`font-semibold p-2 border-4 border-black cursor-pointer my-4 rounded-lg `}>{item}</li>
+            <li key={idx} onClick={(e) => checkanswer(e, item)} className={`font-semibold p-2 border-4 border-black cursor-pointer my-4 rounded-lg ${Answer === item && rightanswercolor} ${Answer !== item && wronganswercolor}`}>{item}</li>
           ))}
         </ul>
         {
@@ -42,15 +50,30 @@ const Quiz = () => {
             <button onClick={() => {
               setLock(false)
               setIndex(index + 1)
-              li.classList.remove("bg-red-500","bg-green-500")
+              setrightanswercolor('')
+              setwronganswercolor('')
             }} className='btn w-full bg-blue-400 py-2 px-5 font-bold  rounded-full '>Next</button>
             :
-            <button className='btn w-full bg-green-400 py-2 px-5 font-bold  rounded-full '>Check result</button>
-        }
-        <p className='text-center my-2 md:text-lg font-mono font-bold'>
+            <button className='btn w-full bg-green-400 py-2 px-5 font-bold  rounded-full' onClick={() => setresult(true)}>Submit</button>
+          }
+          <p className='text-center my-2 md:text-lg font-mono font-bold'>
           {index + 1} out of {Questions.length}
         </p>
       </div>
+      :
+        <div>
+        <h1 className='text-4xl font-serif'>Your scored {score} out of {Questions.length}</h1>
+        <button className='btn w-full mt-4 bg-red-400 py-2 px-5 font-bold  rounded-full' onClick={() => {
+          setIndex(0)
+          setLock(false)
+          setscore(0)
+          setrightanswercolor('')
+          setwronganswercolor('')
+          setresult(false)
+        }}>Reset</button>
+        
+        </div>
+        }
     </div>
   )
 }
